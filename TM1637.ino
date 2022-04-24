f30fc16e61c6b27a8988840ad8d5a61b20abf324
BintangPascallo <92497118+BintangPascallo@users.noreply.github.com>
2022-04-25 00:30:30 +0700

    Update TM1637.ino

--- TM1637.ino
+++ TM1637.ino
@@ -19,3 +19,40 @@ void loop()
     delay(100);  
   }
 }
+
+Atau
+
+#include <TM1637.h>
+
+int CLK = 2;
+int DIO = 3;
+
+TM1637 tm(CLK,DIO);
+
+void setup() {
+  // put your setup code here, to run once:
+  tm.init();
+
+  //set brightness; 0-7
+  tm.set(2);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // example: "12:ab"
+  // tm.display(position, character);
+ 
+
+  // example: "1234"
+  displayNumber(1234);
+
+  delay(1000);
+}
+
+void displayNumber(int num){   
+    tm.display(3, num % 10);   
+    tm.display(2, num / 10 % 10);   
+    tm.display(1, num / 100 % 10);   
+    tm.display(0, num / 1000 % 10);
+}
